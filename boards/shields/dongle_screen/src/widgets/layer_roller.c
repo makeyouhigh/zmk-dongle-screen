4913c2abcc9c9b64aea46d4e5fa25b0ca5470908
makeyouhigh <73630857+makeyouhigh@users.noreply.github.com>
2026-03-07 18:55:32 +0900

    Update layer_roller.c

--- boards/shields/dongle_screen/src/widgets/layer_roller.c
+++ boards/shields/dongle_screen/src/widgets/layer_roller.c
@@ -167,7 +167,7 @@ int zmk_widget_layer_roller_init(struct zmk_widget_layer_roller *widget, lv_obj_
     }
 
     lv_roller_set_options(widget->obj, layer_names_buffer, LV_ROLLER_MODE_NORMAL);
-    lv_roller_set_visible_row_count(widget->obj, 3);
+    lv_roller_set_visible_row_count(widget->obj, 5);
     
     //lv_obj_add_event_cb(widget->obj, mask_event_cb, LV_EVENT_ALL, NULL);
     
