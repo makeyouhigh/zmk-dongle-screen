f1a2c2dbb5d9c81c5e9361a8c2a0ebe10b4443fb
makeyouhigh <73630857+makeyouhigh@users.noreply.github.com>
2026-03-07 18:07:47 +0900

    Update layer_status.c

--- boards/shields/dongle_screen/src/widgets/layer_status.c
+++ boards/shields/dongle_screen/src/widgets/layer_status.c
@@ -96,6 +96,8 @@ int zmk_widget_layer_status_init(struct zmk_widget_layer_status *widget, lv_obj_
     sys_slist_append(&widgets, &widget->node);
 
 //    widget_layer_status_init();
+    _widget_layer_status_init();
+
     return 0;
 }
 
