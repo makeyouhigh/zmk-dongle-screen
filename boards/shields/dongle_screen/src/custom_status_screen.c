005572fe2028c00bbf524602a184972760bf4974
makeyouhigh <73630857+makeyouhigh@users.noreply.github.com>
2026-03-07 18:18:50 +0900

    Update custom_status_screen.c

--- boards/shields/dongle_screen/src/custom_status_screen.c
+++ boards/shields/dongle_screen/src/custom_status_screen.c
@@ -92,7 +92,10 @@ lv_obj_t *zmk_display_status_screen()
 
 #if CONFIG_DONGLE_SCREEN_LAYER_ACTIVE
     zmk_widget_layer_roller_init(&layer_roller_widget, screen);
-    lv_obj_align(zmk_widget_layer_roller_obj(&layer_roller_widget), LV_ALIGN_LEFT_MID, 10, 0);
+//    lv_obj_align(zmk_widget_layer_roller_obj(&layer_roller_widget), LV_ALIGN_LEFT_MID, 10, 0);
+//wpm 위젯 빼고, 레이어 위젯 위치 위로    
+    lv_obj_align(zmk_widget_layer_roller_obj(&layer_roller_widget), LV_ALIGN_LEFT_MID, 10, 10);
+
 #endif
 
 #if CONFIG_DONGLE_SCREEN_MODIFIER_ACTIVE
