da9c2e3eb797e09a3575b9f95065b7f6a167f11e
makeyouhigh <73630857+makeyouhigh@users.noreply.github.com>
2026-03-07 18:38:24 +0900

    Update layer_roller.c

--- boards/shields/dongle_screen/src/widgets/layer_roller.c
+++ boards/shields/dongle_screen/src/widgets/layer_roller.c
@@ -176,7 +176,7 @@ int zmk_widget_layer_roller_init(struct zmk_widget_layer_roller *widget, lv_obj_
     sys_slist_append(&widgets, &widget->node);
     
 //    widget_layer_roller_init();
-    _widget_layer_status_init();
+    _widget_layer_roller_init();
     return 0;
 }
 
